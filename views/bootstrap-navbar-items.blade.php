da9803896c2aa4be85ccd856a3c193f47a2951d7
Kiwina <steven@kleinert.co>
2015-04-21 14:40:55 +0800

    dont escape title

--- views/bootstrap-navbar-items.blade.php
+++ views/bootstrap-navbar-items.blade.php
@@ -1,17 +1,17 @@
 @foreach($items as $item)
   <li@lm-attrs($item) @if($item->hasChildren())class ="dropdown"@endif @lm-endattrs>
     @if($item->link) <a@lm-attrs($item->link) @if($item->hasChildren()) class="dropdown-toggle" data-toggle="dropdown" @endif @lm-endattrs href="{{ $item->url() }}">
-      {{ $item->title }}
+      {!! $item->title !!}
       @if($item->hasChildren()) <b class="caret"></b> @endif
     </a>
     @else
-      {{$item->title}}
+      {!! $item->title !!}
     @endif
     @if($item->hasChildren())
       <ul class="dropdown-menu">
-        @include('laravel5-menu::bootstrap-navbar-items',
+        @include('laravel5-menu::bootstrap-navbar-items', 
           array('items' => $item->children()))
-      </ul>
+      </ul> 
     @endif
   </li>
   @if($item->divider)
